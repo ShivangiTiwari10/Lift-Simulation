96c3986f80844fb72efa6d170141d91be3ae5f0a
Shivangi <shivangit1111@gmail.com>
2023-07-31 15:45:47 +0530

    feat: on button click send user to next page

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 const upButton = document.querySelector(".submmitButton");
 // upButton.addEventListener
+
+document.addEventListener("DOMContentLoaded", function () {
+  const submitButton = document.querySelector(".submitButton");
+
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    const numLifts = document.getElementById("liftnumber").value;
+    const numFloors = document.getElementById("floornumber").value;
+
+    if (numLifts.trim() === "" || numFloors.trim() === "") {
+      alert("Please fill in both input boxes.");
+    } else {
+      window.location.href =
+        "lift.html?lifts=" + numLifts + "&floors=" + numFloors;
+    }
+  });
+});
