308fd2c696a115e25274bcc7bb768e3da81e3c14
Shivangi <shivangit1111@gmail.com>
2023-08-01 09:55:01 +0530

    feat: add condition for input

--- src/main.js
+++ src/main.js
@@ -1,20 +1,28 @@
-const upButton = document.querySelector(".submmitButton");
-// upButton.addEventListener
-
 document.addEventListener("DOMContentLoaded", function () {
   const submitButton = document.querySelector(".submitButton");
 
   submitButton.addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault(); 
 
     const numLifts = document.getElementById("liftnumber").value;
     const numFloors = document.getElementById("floornumber").value;
 
     if (numLifts.trim() === "" || numFloors.trim() === "") {
       alert("Please fill in both input boxes.");
-    } else {
+   
+    } else if(numLifts<0|| numFloors<0){
+      alert("Enter positive numbers")
+      return false;
+
+    }else if(numLifts>numFloors){
+      alert("no. of lift can not be more than no. of floors")
+      return false 
+
+    }
+      else {
       window.location.href =
-        "lift.html?lifts=" + numLifts + "&floors=" + numFloors;
+        "lift.html?lifts" 
+     
     }
   });
 });
