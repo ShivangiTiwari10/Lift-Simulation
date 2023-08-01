cde5874ef078f5a93e59778bcd440c17a13ac2f0
Shivangi <shivangit1111@gmail.com>
2023-08-01 18:58:54 +0530

    refactor: make changes in style

--- src/main.js
+++ src/main.js
@@ -2,27 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const submitButton = document.querySelector(".submitButton");
 
   submitButton.addEventListener("click", function (event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
     const numLifts = document.getElementById("liftnumber").value;
     const numFloors = document.getElementById("floornumber").value;
 
     if (numLifts.trim() === "" || numFloors.trim() === "") {
       alert("Please fill in both input boxes.");
-   
-    } else if(numLifts<0|| numFloors<0){
-      alert("Enter positive numbers")
+    } else if (numLifts < 0 || numFloors < 0) {
+      alert("Enter positive numbers");
       return false;
-
-    }else if(numLifts>numFloors){
-      alert("no. of lift can not be more than no. of floors")
-      return false 
-
-    }
-      else {
-      window.location.href =
-        "lift.html?lifts" 
-     
+    } else if (numLifts > numFloors) {
+      alert("no. of lift can not be more than no. of floors");
+      return false;
+    } else {
+      return true;
     }
   });
 });
