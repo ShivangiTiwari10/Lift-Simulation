61b1774c5b309631078f3798c66dd249d20dee2d
Shivangi <shivangit1111@gmail.com>
2023-08-03 11:18:03 +0530

    get inputed data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const submitButton = document.querySelector(".submitButton");
 
+  const formContainer = document.getElementById("form");
+
+  const liftSimulationContainer = document.querySelector(".floor-wrapper");
+  //   const buttonContainer = document.querySelector(".button-container");
+  const floorContainer = document.getElementById("floorContainer");
+
   submitButton.addEventListener("click", function (event) {
     event.preventDefault();
 
@@ -14,9 +20,29 @@ document.addEventListener("DOMContentLoaded", function () {
       return false;
     } else if (numLifts > numFloors) {
       alert("no. of lift can not be more than no. of floors");
-      return false;
-    } else {
-      return true;
+      return;
+    }
+
+    // Hide the input form
+    formContainer.style.display = "none";
+
+    // Show the lift simulation UI
+
+    liftSimulationContainer.style.display = "block";
+    // buttonContainer.style.display = "flex";
+
+    floorContainer.innerHTML = "";
+
+    for (let i = 1; i <= numFloors; i++) {
+      const floorElement = document.createElement("div");
+      floorElement.className = "floor";
+
+      floorElement.innerHTML = `
+      <button class="floor-button up-button">↑</button>
+      <span class="floor-number">Floor ${i}</span>
+      <button class="floor-button down-button">↓</button>
+    `;
+      floorContainer.appendChild(floorElement);
     }
   });
 });
